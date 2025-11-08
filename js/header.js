40771d0f05128db0845bcc583a3cdca540702c32
NosiphoX <2708798@students.wits.ac.za>
2025-11-08 21:00:50 +0200

    hamburger menu

---
+++ js/header.js
@@ -0,0 +1,9 @@
+// js/header.js
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".main-nav");
+
+  menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+});
